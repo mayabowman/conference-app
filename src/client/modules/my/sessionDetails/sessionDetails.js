8d01ef9d0d39158a805604b4c07207da1a812c26
Maya Bowman <mbowman@shift7digital.com>
2020-10-29 13:39:31 -0400

    added navigation between list and details

--- src/client/modules/my/sessionDetails/sessionDetails.js
+++ src/client/modules/my/sessionDetails/sessionDetails.js
@@ -1,6 +1,6 @@
 import { LightningElement, api } from 'lwc';
 import { getSession } from 'data/sessionService';
-export default class SessionDatilas extends LightningElement {
+export default class SessionDetails extends LightningElement {
     session;
     @api
     set sessionId(id) {
@@ -9,5 +9,13 @@ export default class SessionDatilas extends LightningElement {
     }
     get sessionId() {
         return this._sessionId;
+    }
+    handleSessionsClick() {
+        const navigateEvent = new CustomEvent('navigate', {
+            detail: {
+                state: 'list'
+            }
+        });
+        this.dispatchEvent(navigateEvent);
     }
 }
